a23fa8021e1ba53014ab200cecd2a7690299fdd5
Mohammedfazil <mohammedfazil20005@gmail.com>
2024-12-15 13:05:40 +0530

    first commit

---
+++ console.js
@@ -0,0 +1,139 @@
+
+// // //ayush question//
+// // let i
+
+// // for(i=1000;i<=1500;i++){
+// //     if(i%5===0&&i%25===0){
+// //         console.log(i);
+        
+// //     }
+// // }
+
+// // //javed question//
+
+// // let i=1;
+
+// // do{
+// //     if(i%2===0){
+// //         console.log(i)
+// //     }
+// //     i++
+// // }while(i<=50)
+
+
+// // //prezelt question
+
+
+
+// // let number = 5;  
+// // let factorial = 1;
+// // let i;
+
+// // for (i = 1; i <= number; i++) {
+// //     factorial *= i;
+// // }
+
+
+// // console.log("The factorial of " + number + " is " + factorial);
+
+// // //anandhu question
+// // let i=1
+
+// // while(i<=100){
+// //     if(i%3==0){
+// //         console.log(i)
+// //     }
+// //     i++
+// // }
+
+// // //archa question
+// // let i;
+// // let sum=0
+// // for(i=1;i<=10;i++){
+// //      sum+=i
+  
+// // }
+// // console.log(sum)
+
+
+// // let first =0;
+// // let second=1;
+
+// // let i;
+
+// // for(i=0;i<=10;i++){
+// //     console.log(first)
+
+// //     let next=first+second
+
+// //     first=second
+// //     second=next
+
+// // }
+
+
+// // //shereena
+// // let number=8
+// // let i;
+
+// // for(i=1;i<=10;i++){
+// //    let result=number*i
+// //     console.log(result)
+// // }
+
+
+// // //chandana//
+
+
+// // let i;
+
+// // for(i=200;i<=500;i++){
+// //     if(i%2==0){
+// //         console.log(i)
+// //     }
+// // }
+
+
+// // //sreepriya//
+
+// // let i;
+
+// // for(i=1;i<=50;i++){
+// //     if(i%2!=0){
+// //         console.log(i)
+// //     }
+// // }
+
+// //abhjiTH
+// // let keys=["name","age","place","hobbie","hobbie2"]
+// // let values=["fazil","18","trivandrum","coding","sleeping"]
+
+// // let i;
+// // let obj={}
+
+// // for(i=0;i<keys.length;i++){
+// //     obj[keys[i]]=values[i]
+// // }
+// // console.log(obj)
+
+// // //my
+// // let i;
+
+// // for(i=1;i<=100;i++){
+// //     if(i%2==0){
+// //         console.log(i)
+// //     }
+// // }
+
+// for(let num=2;num<=100;num++){
+//     let isPrime=true
+//     for(let i=2;i<num;i++){
+//         if(num%i==0){
+//             isPrime=false
+//             break
+//         }
+//     }
+//     if(isPrime){
+//         console.log(num)
+//     }
+// }
